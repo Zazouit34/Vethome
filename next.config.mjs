72031713d6cdc86cb1ed99848794cb0a64cfd5bd
Zazouit34 <zazouitmed@gmail.com>
2025-04-25 20:13:42 +0100

    fix the calendar ui

---
+++ next.config.mjs
@@ -0,0 +1,11 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  output: 'export',
+  basePath: '/Vethome',
+  assetPrefix: '/Vethome/',
+  images: {
+    unoptimized: true,
+  },
+};
+
+export default nextConfig; 
